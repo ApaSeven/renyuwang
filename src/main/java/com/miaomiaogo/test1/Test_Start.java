036bf6cc9c58fb8f694669ec35ea8f4f61800c73
apaSeven <397572391@qq.com>
2018-04-06 18:58:18 +0800

    1

---
+++ src/main/java/com/miaomiaogo/test1/Test_Start.java
@@ -0,0 +1,35 @@
+package com.miaomiaogo.test1;
+
+
+import java.util.ArrayList;
+
+/**
+ * Created by acer on 2018/4/5.
+ */
+public class Test_Start {
+    private int age;
+
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello_miaomiaogo");
+
+        int temp1 = 10;
+        int temp2 = 20;
+        int[] array1 = new int[10];
+
+        ArrayList<String> array2 = new ArrayList<String>();
+
+        for (int i = 0; i < array1.length; i++) {
+            int i1= array1[i];
+            System.out.println(i1);
+        }
+
+
+        for (String s : array2) {
+
+        }
+
+
+    }
+}
